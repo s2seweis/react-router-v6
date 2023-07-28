0adc8d219f58140bc423f3a7e0b537f0240f6845
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-07-28 06:39:10 +0200

    upload

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from 'react';
+
+import {Row, Col, Form, Input} from 'antd';
+
+import 'antd/dist/antd.min.css';
+
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login">
 
-export default Login
+      <Row gutter={16}>
+
+        <Col lg={8} />
+        <Col lg={8} className='text-left' >
+          <Form style={{marginTop:"100px"}} layout="vertical">
+
+            <h1>Login</h1>
+            <hr></hr>
+            <br></br>
+
+            <Form.Item
+              name="username"
+              label="Username"
+              rules={[{required: true}]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              label="Password"
+              rules={[{required: true}]}
+            >
+              <Input />
+            </Form.Item>
+
+            <button className='btn1'>Login</button>
+
+          </Form>
+        </Col>
+
+      </Row>
+
+    </div>
+  );
+};
+
+export default Login;
